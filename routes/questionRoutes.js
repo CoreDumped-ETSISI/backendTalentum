5c30a08b6e8d9ae27027bad1c64d80c8f2130980
cesaramnuel <cesitar.miko@gmail.com>
2017-11-11 18:56:33 +0100

    Question routes defined

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,7 +1,13 @@
+import { read } from 'fs';
+
 'use strict'
 
 const express = require ('express')
 const api = express.Router()
+const questionController = require ('../controllers/questionController.js')
 
+api.get('/getQuestions', questionController.getQuestions)
+api.get('/getCategoryQuestions', questionController.getCategoryQuestions)
+api.post('/publishQuestion', questionController.publishQuestion)
 
 module.exports = api
