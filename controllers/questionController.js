ee7c891dc2b403250737b4a166f51529a9e8f2da
cesaramnuel <cesitar.miko@gmail.com>
2017-11-11 18:43:23 +0100

    questionController added (properly added now)

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -12,3 +12,34 @@ function getQuestions(req, res){
     })
 }
 
+function getCategoryQuestions (req, res){
+
+    var category = req.params.category
+
+    Question.find({category : category}, (err, questions) => {
+        if(err) return res.status(500).send({message: `Error on request: ${err}` })
+        if(!users) return res.status(404).send({message: `No questions found: ${err}` })
+        
+        return res.status(200).send(questions)
+    })
+}
+
+
+function publishQuestion (req, res) {
+    let question = new Question ()
+        text = req.body.text
+        image = req.body.image
+        category = req.body.category
+        options = req.body.options
+
+    question.save(function (err, question) {
+        if(err) return res.status(500).send( err.message);
+        res.status(200).send(question)
+    })
+}
+
+module.exports = {
+    getQuestions,
+    getCategoryQuestions,
+    publishQuestion
+}
