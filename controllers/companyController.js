cd23870f13325b3f10ba0243183563f3ca6355fa
Xavikh <xavikh@gmail.com>
2017-11-12 20:58:01 +0100

    Some corrections to company and question controllers

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -4,40 +4,44 @@ const Company = require ('../models/company.js')
 const Offer = require ('../models/offer.js')
 
 function getCompany (req, res){
-
     var companyName = req.params.companyName
 
     Company.find({name : companyName}, (err, company) => {
         if(err) return res.status(500).send({message: `Error on request: ${err}` })
-        if(!users) return res.status(404).send({message: `No company found: ${err}` })
-        
+        if(!company) return res.status(404).send({message: `No company found: ${err}` })
+
         return res.status(200).send(company)
     })
 }
 
-
 function createCompany (req, res) {
-    let company = new Company ()
-    name = req.body.name,
-    address = req.body.address,
-    email = req.body.email,
-    offers = []
-
-    company.save(function (err, company) {
-        if(err) return res.status(500).send( err.message);
-        res.status(200).send(company)
-    })
+  var name = req.body.name
+  var address = req.body.address
+  var email = req.body.email
+  var offers = []
+  var company = new Company({
+    name: name,
+    address: address,
+    email: email,
+    offers: offers
+  })
+  company.save(function (err, company) {
+      if(err) return res.status(500).send( err.message);
+      res.status(200).send(company)
+  })
 }
+
 function createOffer (req, res) {
     var companyName = req.params.companyName
 
-    var receivedOffer = new Offer ()
-        title = req.body.title,
-        payout = req.body.payout,
-        companyName = req.body.companyName,
-        sector = req.body.sector,
-        description = req.body.description,
-        tasks = req.body.tasks
+    var receivedOffer = new Offer ({
+        title: req.body.title,
+        payout: req.body.payout,
+        companyName: req.body.companyName,
+        sector: req.body.sector,
+        description: req.body.description,
+        tasks: req.body.tasks
+      })
 
     Company.find({name : companyName}, (err, company) => {
         if(err) return res.status(500).send({message: `Error at processing request: ${err}`})
--- controllers/questionController.js
+++ controllers/questionController.js
@@ -3,37 +3,36 @@
 const Question = require ('../models/question.js')
 
 function getQuestions(req, res){
-    
-    Question.find({}, (err, users) => {
+    Question.find({}, (err, question) => {
         if(err) return res.status(500).send({message: `Error on request: ${err}` })
-        if(!users) return res.status(404).send({message: `No questions found: ${err}` })
+        if(!question) return res.status(404).send({message: `No questions found: ${err}` })
 
-        return res.status(200).send(users)
+        return res.status(200).send(question)
     })
 }
 
 function getCategoryQuestions (req, res){
-
     var category = req.params.category
 
     Question.find({category : category}, (err, questions) => {
         if(err) return res.status(500).send({message: `Error on request: ${err}` })
-        if(!users) return res.status(404).send({message: `No questions found: ${err}` })
-        
+        if(!questions) return res.status(404).send({message: `No questions found: ${err}` })
+
         return res.status(200).send(questions)
     })
 }
 
 
 function publishQuestion (req, res) {
-    let question = new Question ()
-        text = req.body.text
-        image = req.body.image
-        category = req.body.category
-        options = req.body.options
+    let question: new Question ({
+        text: req.body.text
+        image: req.body.image
+        category: req.body.category
+        options: req.body.options
+      })
 
     question.save(function (err, question) {
-        if(err) return res.status(500).send( err.message);
+        if(err) return res.status(500).send(err.message);
         res.status(200).send(question)
     })
 }
